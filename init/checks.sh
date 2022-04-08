e289c3ea5a03c9562559a3a184a788551fc90969
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2022-04-08 19:50:20 +0530

    Update checks.sh

--- init/checks.sh
+++ init/checks.sh
@@ -51,7 +51,6 @@ _checkDefaultVars() {
         [PREFERRED_LANGUAGE]="en"
         [DOWN_PATH]="downloads"
         [UPSTREAM_REMOTE]="upstream"
-        [UPSTREAM_REPO]="https://github.com/code-rgb/USERGE-X"
         [LOAD_UNOFFICIAL_PLUGINS]=true
         [CUSTOM_PLUGINS_REPO]=""
         [G_DRIVE_IS_TD]=true
