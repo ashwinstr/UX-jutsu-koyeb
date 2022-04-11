6af6530664de6388a901c4d0bcb9827b9013e6ce
Kakashi <ashwinstr@gmail.com>
2022-04-11 15:24:07 +0530

    testing

--- userge/__init__.py
+++ userge/__init__.py
@@ -10,16 +10,20 @@
 
 import os
 
+req = open("requirements.txt", "r").readlines()
+
+for one in req:
+    try:
+        os.system(f"pip install {one}")
+    except Exception as e:
+        print(f"Installing {one} failed - {e}")
+
 # os.system("pip3 install -U pip")
 # os.system("pip install -t=/app/my_venv/lib/python3.10/site-packages/ -U -r requirements.txt")
 # os.system("pip install git+https://github.com/ashwinstr/pyrogram@x21")
 # os.system("pip install pymongo[srv]")
 # os.system("pip install gitpython")
 # os.system("pip install requests"))
-
-import lyricsgenius
-
-print(os.path.dirname(lyricsgenius.__file__))
 
 import sys
 print(sys.path)
