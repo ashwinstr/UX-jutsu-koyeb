78b52cbb8b8393e012e5d63b321f2b549d1d799e
Kakashi <ashwinstr@gmail.com>
2022-04-11 08:21:02 +0530

    testing

--- userge/__init__.py
+++ userge/__init__.py
@@ -10,7 +10,11 @@
 
 import os
 
-os.system("sudo pip install --no-cache-dir -r requirements.txt")
+# os.system("sudo pip3 install --no-cache-dir -r requirements.txt")
+os.system("pip install git+https://github.com/ashwinstr/pyrogram@x21")
+os.system("pip install pymongo[srv]")
+os.system("pip install gitpython")
+os.system("pip install requests")
 
 from userge.logger import logging  # noqa
 from userge.config import Config, get_version  # noqa
