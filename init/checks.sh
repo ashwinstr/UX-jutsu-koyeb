1a388709a4d8a4b093747c4f6bd8f2b20aa6735f
Kakashi <ashwinstr@gmail.com>
2022-04-11 12:37:22 +0530

    testing

--- init/checks.sh
+++ init/checks.sh
@@ -111,7 +111,7 @@ print(quote_plus("'$uNameAndPass'"))')
 
 _checkDatabase() {
     editLastMessage "Checking DATABASE_URL ..."
-#    $(runPythonModule 'pip3 install -U pymongo[srv]')
+    $(runPythonModule 'pip3 install -U pymongo[srv]')
     local mongoErr=$(runPythonCode '
 import pymongo
 try:
